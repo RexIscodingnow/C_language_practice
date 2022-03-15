1083b9c3d65ab17c8cf988bd408996bcd42640a9
RexIscodingnow <90883713+RexIscodingnow@users.noreply.github.com>
2022-03-15 14:05:46 +0800

    Add files via upload

---
+++ c_lang/triangle_up_left.c
@@ -0,0 +1,28 @@
+/*
+    左上方之三角形
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[]) {
+
+    int i, j, n;
+    printf("n = ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            if (i == j) {
+                printf("*");
+            }
+            else if (i < j) {
+                printf("*");
+            }
+        }
+        printf("\n");
+    }
+
+    system("pause");
+    return 0;
+}
+
